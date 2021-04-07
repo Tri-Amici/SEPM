e9581a467256c64364e4bed8311115407e3a2a23
Matt Kellock <matt@polr.software>
2021-04-07 10:54:30 +1000

    SEPM-2: Initial Java project

---
+++ src/TriAmici.java
@@ -0,0 +1,15 @@
+/**
+ * @author Assignment Group 9
+ *
+ */
+public class TriAmici {
+
+	/**
+	 * Main application method
+	 */
+	public static void main(String[] args) {
+		// Mock input to validate project works
+		System.out.println("Working!");
+	}
+
+}
