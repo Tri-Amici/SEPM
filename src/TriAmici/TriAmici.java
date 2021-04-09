ddad8b5ab4f75c799bb62321a6d89edb8934ac42
Matt Kellock <matt@polr.software>
2021-04-09 19:41:24 +1000

    Adding Eclipse support to code

--- src/TriAmici/TriAmici.java
+++ src/TriAmici/TriAmici.java
@@ -12,8 +12,16 @@ public class TriAmici {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		
-		Storage test = new Storage();
+		// Run in Eclipse default
+		boolean runInEclipse = false;
 		
+		// Check if we're running in Eclipse
+		for(int i = 0; i < args.length; i++)
+			if (args[0].contains("-runInEclipse"))
+				runInEclipse = true;
+		
+		// Create a new instance of the Storage class
+		Storage test = new Storage(runInEclipse);
 		
 		System.out.println("Working!");
 	}
